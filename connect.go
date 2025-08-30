944bfbae1c04f3b6f3616d7e09688fa653450de3
npezzotti <nathan.pezzotti@hashicorp.com>
2025-08-30 13:05:49 -0400

    new closeWrite

--- connect.go
+++ connect.go
@@ -47,7 +47,7 @@ func (n *netcat) connect(network, remoteAddr string) error {
 			go func() {
 				err := scanLinesWithInterval(conn, n.stdin, n.cfg.Interval)
 				if err == nil && !n.cfg.NoShutdown {
-					err = closeWrite(conn)
+					err = closeWrite(conn.(WriteCloser))
 				}
 				writeErrChan <- err
 			}()
@@ -55,7 +55,7 @@ func (n *netcat) connect(network, remoteAddr string) error {
 			go func() {
 				_, err := io.Copy(newIdleTimeoutConn(conn, n.cfg.Timeout), n.stdin)
 				if err == nil && !n.cfg.NoShutdown {
-					err = closeWrite(conn)
+					err = closeWrite(conn.(WriteCloser))
 				}
 				writeErrChan <- err
 			}()
